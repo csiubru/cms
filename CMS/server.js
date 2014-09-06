acb5f61071270ad89f941cb91d11bdffe2391961
CSIUBRU <prayong172@gmail.com>
2014-09-06 13:57:26 +0700

    server.js finished

--- CMS/server.js
+++ CMS/server.js
@@ -1,23 +1,21 @@
 var express = require('express'),
     path = require('path'),
-    favicon = require('serve-favicon'), //06-09-14
-    logger = require('morgan'),         //06-09-14
-    cookieParser = require('cookie-parser'),    //06-09-14
-    bodyParser = require('body-parser'),        //06-09-14
     http = require('http'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
     wine = require('./routes/wines');
 
 var app = express();
 
-// app.configure(function () { //remove for new version of configuration 06-09-14
+
     app.set('port', process.env.PORT || 3000);
-//    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
-//    app.use(express.bodyParser()), //new version of body-parser
+    app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
-// }); //new version of configuration 06-09-14
+
 
 app.get('/wines', wine.findAll);
 app.get('/wines/:id', wine.findById);
