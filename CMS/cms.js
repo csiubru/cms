49c864fe2b655c32631aa86e9c10874cc4107d98
CSIUBRU <prayong172@gmail.com>
2014-09-07 08:57:32 +0700

    set directory

--- CMS/cms.js
+++ CMS/cms.js
@@ -25,4 +25,5 @@ app.delete('/courses/:id', course.deleteCourse);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
+    console.log("Waiting for connect to DB...");
 });
